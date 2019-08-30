db0f1cb4c8948ac817c4af9867d692fc30af4963
michal milke <desmilke@gmail.com>
2019-08-30 11:39:51 +0200

    commit zadania javascript 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+/*
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + 'papier' + '! Jeśli Twój ruch to kamień, to wygrywam!');
+printMessage('Zagrałem ' + 'nożyce' + '! Jeśli Twój ruch to papier, to przegrywasz!');
+printMessage('Zagrałem ' + 'nożyce' + '! Jeśli Twój ruch to kamień, to wygrywasz!'); 
+*/
