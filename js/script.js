176c313bdc9d999d23690c40e6cc333977fde99a
michal milke <desmilke@gmail.com>
2019-08-31 20:49:11 +0200

    changes with wrong picks solved

--- js/script.js
+++ js/script.js
@@ -5,66 +5,79 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
-  computerMove = 'kamień';
+  computerMove = 'stone';
 }
 else if(randomNumber == 2) {
-  computerMove = 'papier';
+  computerMove = 'paper';
 }
 else if(randomNumber == 3) {
-  computerMove = 'nożyce';
+  computerMove = 'scissors';
 }
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Computer choice is : ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let playerInput = prompt('Pick your move! 1: stone, 2: paper, 3: scissors.');
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Player has chosen: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = 'uncommon move';
 
 if(playerInput == '1'){
-  playerMove = 'kamień';
+  playerMove = 'stone';
 }
 
 else if(playerInput == '2'){
-    playerMove = 'papier';
+    playerMove = 'paper';
 }
 
 else if(playerInput == '3'){
-    playerMove = 'nożyce';
+    playerMove = 'scissors';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+else if(playerInput != '1' && playerInput != '2' && playerInput != '3') {
+    printMessage('Wrong symbol!!! ' + 'Choose again');
+}
+//czy wariant poniżej może być tak zakodowany?
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  }
- if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Ty przegrywasz!');
-  }
+else if(playerInput >3 && playerInput <4) {
+    printMessage('Incorrect pick!!!');
 
-  if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Macie remis!');
-  }
 
-  if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-  }
- if( computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Macie remis!');
-  }
+}
 
-  if( computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Ty przegrywasz!');
+
+
+printMessage('Player move: ' + playerMove);
+
+if( computerMove == 'stone' && playerMove == 'paper'){
+    printMessage('Player won!');
+  }
+ if( computerMove == 'stone' && playerMove == 'scissors'){
+    printMessage('Player loose!');
   }
 
-  if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Macie remis!');
-  }
- if( computerMove == 'nożyce' && playerMove == 'papier'){
-    printMessage('Ty przegrywasz!');
+  if( computerMove == 'stone' && playerMove == 'stone'){
+    printMessage('Tie!');
   }
 
-  if( computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
+  if( computerMove == 'paper' && playerMove == 'scissors'){
+    printMessage('Player win!');
+  }
+ if( computerMove == 'paper' && playerMove == 'paper'){
+    printMessage('Tie!');
+  }
+
+  if( computerMove == 'paper' && playerMove == 'stone'){
+    printMessage('Player loose!');
+  }
+
+  if( computerMove == 'scissors' && playerMove == 'scissors'){
+    printMessage('Tie!');
+  }
+ if( computerMove == 'scissors' && playerMove == 'paper'){
+    printMessage('Player loose!');
+  }
+
+  if( computerMove == 'scissors' && playerMove == 'stone'){
+    printMessage('Player win!');
   }
 
